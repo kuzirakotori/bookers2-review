0f6cc79b7e0e0c325b1b1ff879be5fae31073264
EC2 Default User <ec2-user@ip-172-31-6-144.ap-northeast-1.compute.internal>
2022-06-22 01:49:57 +0000

    [Update]ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,8 @@ Rails.application.routes.draw do
     get "event_notices" => "event_notices#sent"
   end 
   resources :chats, only: [:show, :create]
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
